44d0fd212ba193579e242a9eb56796480a3e8468
StefanRaluca <raluca.bubulina@yahoo.com>
2024-02-23 17:10:09 +0100

    add  function button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 console.log("works");
 
-//const per img con array
+// const per img con array
 const imgCarousel = [
     "./assets/img/01.webp",
     "./assets/img/02.webp",
@@ -9,20 +9,60 @@ const imgCarousel = [
     "./assets/img/05.webp"
 ];
 
-//selezionare le img che sono dentro il carousel con una const/let
+// selezionare le img che sono dentro il carousel con una const/let
 
-const activeImg = 3;
-const carousel = document.querySelector('.carousel_img')
+let activeImg = 3;
+const carousel = document.querySelector('.carousel_img');
 console.log(imgCarousel, activeImg, carousel);
 
-//cicliamo (loop)
+// const per i button
+const upButton = document.querySelector('.top_button');
+const downButton = document.querySelector('.bottom_button');
+
+
+
+// cicliamo (loop)
 
 for (let i = 0; i < imgCarousel.length; i++) {
     const elementCarousel = imgCarousel[i];
     console.log(elementCarousel);
-//selezioniamo il markup e facciamo una condizione per che la classe sia assegnata 
-    const markupCarousel = `<img class="${i === activeImg ? 'active' : ''}" src="${elementCarousel}" ></img>`
+    // selezioniamo il markup e facciamo una condizione per che la classe sia assegnata 
+    const markupCarousel = `<img class="${i === activeImg ? 'active' : ''}" src="${elementCarousel}" ></img>`;
     console.log(markupCarousel);
 
-    carousel.insertAdjacentHTML('beforeend', markupCarousel)
+    carousel.insertAdjacentHTML('beforeend', markupCarousel);
 }
+
+console.log(upButton);
+console.log(downButton);
+
+// facciamo funzionare i button 
+upButton.addEventListener('click', function () {
+    console.log('up button');
+    activeImg++
+    console.log(activeImg);
+
+    const currentImg = document.querySelector('.active');
+
+    // Rimuovi la classe 'active' dall'immagine corrente
+    currentImg.classList.remove('active');
+
+    const allImg = document.querySelectorAll('.carousel_img img');
+    
+    allImg[activeImg].classList.add('active');
+});
+
+downButton.addEventListener('click', function () {
+    console.log('down button');
+    activeImg--
+    console.log(activeImg);
+
+    const currentImg = document.querySelector('.active');
+
+    // Rimuovi la classe 'active' dall'immagine corrente
+    currentImg.classList.remove('active');
+
+    const allImg = document.querySelectorAll('.carousel_img img');
+    
+    allImg[activeImg].classList.add('active');
+});
